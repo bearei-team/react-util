db3fd1b0bea28f03bff2d8f01d8764e6a3893357
Bearei <bear_ei@outlook.com>
2022-11-22 10:29:01 +0800

    feat(userEvent): add event handling.

--- test/userEvent.test.tsx
+++ test/userEvent.test.tsx
@@ -1,10 +1,21 @@
-import {getPlatformClickEvent} from '../src/userEvent';
+import {
+  getPlatformClickEvent,
+  HandleClickEvent,
+  handleEvent,
+} from '../src/userEvent';
 
 describe('test/userEvent.test.ts', () => {
+  test('It should supposed to be an event', async () => {
+    const clickEvent = handleEvent(() => 'callback');
+
+    expect(typeof clickEvent).toEqual('function');
+    expect(clickEvent({})).toEqual('callback');
+  });
+
   test('It should be the capture platform event', async () => {
     const clickEvent = getPlatformClickEvent(() => 'callback');
 
     expect(typeof clickEvent['onClick']).toEqual('function');
-    expect(clickEvent['onClick']?.()).toEqual('callback');
+    expect(clickEvent['onClick']?.({} as HandleClickEvent)).toEqual('callback');
   });
 });
