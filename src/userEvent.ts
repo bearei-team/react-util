2aed3264a4aa182586d5fca6cae7a527922cebae
Bearei <bear_ei@outlook.com>
2022-11-22 10:30:01 +0800

    feat(userEvent): added get platform event function. (#10)
    * chore: initialize
    
    * feat(userEvent): added get platform event function.
    
    * feat: version 0.0.3
    
    * fix(getPlatform): fix the React Native runtime judgment failure.
    
    * fix(getPlatform): fix the React Native runtime judgment failure
    
    * fix(getPlatform): fix type assertion error.
    
    * chore(package): update release configuration.
    
    * feat(userEvent): add type definition.
    
    * feat: version 0.0.7
    
    * fix: fixed an event handling a failed binding.
    
    * chore: fix TS plug-in configuration error.
    
    * feat(userEvent): add event handling.

--- src/userEvent.ts
+++ src/userEvent.ts
@@ -1,13 +1,26 @@
 import {GestureResponderEvent} from 'react-native';
 import {getPlatform} from './getPlatform';
 
+export type DefaultEvent = {
+  preventDefault?: () => void;
+  stopPropagation?: () => void;
+};
+
 export type OnClickEvent = 'onClick' | 'onPress' | 'onTouchEnd';
 export type HandleClickEvent =
   | React.MouseEvent<HTMLElement, MouseEvent>
   | React.TouchEvent<HTMLElement>
   | GestureResponderEvent;
 
-export type ClickEventFun = (e?: HandleClickEvent) => unknown;
+export type ClickEventFun = (e: HandleClickEvent) => void;
+export const handleEvent =
+  <T extends DefaultEvent>(callback?: (e: T) => void) =>
+  (e: T) => {
+    e.preventDefault?.();
+    e.stopPropagation?.();
+
+    return callback?.(e);
+  };
 
 export const getPlatformClickEvent = (eventFun?: ClickEventFun) => {
   const event = {
@@ -16,14 +29,7 @@ export const getPlatformClickEvent = (eventFun?: ClickEventFun) => {
     mobileBrowser: 'onTouchEnd',
   };
 
-  const handleEvent = (callback?: ClickEventFun) => (e?: HandleClickEvent) => {
-    e?.preventDefault();
-    e?.stopPropagation();
-
-    return callback?.(e);
-  };
-
   return {
-    [event[getPlatform()]]: handleEvent(eventFun),
+    [event[getPlatform()]]: handleEvent<HandleClickEvent>(eventFun),
   } as Record<OnClickEvent, ClickEventFun | undefined>;
 };
