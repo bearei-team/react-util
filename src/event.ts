b513107930854a0792b7298406efad5367f19139
Bearei <bear_ei@outlook.com>
2022-12-02 11:03:00 +0800

    feat: Added event binding handling.

--- src/event.ts
+++ src/event.ts
@@ -3,12 +3,10 @@ export type DefaultEvent = {
   stopPropagation?: () => void;
 };
 export interface HandleEvent {
-  <T extends DefaultEvent>(callback?: ((e: T) => void) | undefined): (
-    e: T,
-  ) => void | undefined;
+  <T extends DefaultEvent>(callback?: (e: T) => void): (e: T) => void;
 }
 
-const handleEvent: HandleEvent =
+export const handleDefaultEvent: HandleEvent =
   <T extends DefaultEvent>(callback?: (e: T) => void) =>
   (e: T) => {
     e.preventDefault?.();
@@ -17,4 +15,14 @@ const handleEvent: HandleEvent =
     return callback?.(e);
   };
 
-export default handleEvent;
+export const bindEvents = <T extends string, C = (e: unknown) => void>(
+  names: T[],
+  match: (key: T) => C,
+) => {
+  const events = {} as Record<T, (e: unknown) => void>;
+  const bindEvent = (key: T) => ({[key]: match(key)});
+
+  names.forEach((key: T) => Object.assign(events, bindEvent(key)));
+
+  return events;
+};
